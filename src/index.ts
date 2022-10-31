8d0908ac30170ed86ea368b3ad124410bfcb0035
Haruki <kohei99624@gmail.com>
2022-10-31 08:02:22 -0400

    add fauna functions

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import {
+  faunaClient,
+  faunaQuery,
+  createFaunaDocument,
+  updateFaunaDocument,
+  deleteFaunaDocument,
+  queryAllByFaunaIndexes,
+  queryTermByFaunaIndexes
+} from './faunaDB'
+
+export {
+  faunaClient,
+  faunaQuery,
+  createFaunaDocument,
+  updateFaunaDocument,
+  deleteFaunaDocument,
+  queryAllByFaunaIndexes,
+  queryTermByFaunaIndexes
+}
